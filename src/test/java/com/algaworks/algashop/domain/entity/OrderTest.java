4d7cbcfb0d9603cf60044d4cd684aa95517e99db
anailson <arsantos315@gmail.com>
2026-03-21 20:18:02 -0300

    Evoluindo modelo de BillingInfo

--- src/test/java/com/algaworks/algashop/domain/entity/OrderTest.java
+++ src/test/java/com/algaworks/algashop/domain/entity/OrderTest.java
@@ -112,33 +112,13 @@ class OrderTest {
 
     @Test
     public void givenDraftOrder_whenChangeShipping_shouldAllowChange() {
-        Address address = Address.builder()
-                .street("Bourbon Street")
-                .number("1234")
-                .neighborhood("North Ville")
-                .complement("apt. 11")
-                .city("Montfort")
-                .state("South Carolina")
-                .zipCode(new ZipCode("79911")).build();
 
-        BillingInfo billingInfo = BillingInfo.builder()
-                .address(address)
-                .document(new Document("225-09-1992"))
-                .phone(new Phone("123-111-9911"))
-                .fullName(new FullName("John", "Doe"))
-                .build();
+        Billing billing = OrderTestDataBuilder.aBilling();
 
         Order order = Order.draft(new CustomerId());
-        order.changeBilling(billingInfo);
+        order.changeBilling(billing);
 
-        BillingInfo expectedBillingInfo = BillingInfo.builder()
-                .address(address)
-                .document(new Document("225-09-1992"))
-                .phone(new Phone("123-111-9911"))
-                .fullName(new FullName("John", "Doe"))
-                .build();
-
-        Assertions.assertThat(order.billing()).isEqualTo(expectedBillingInfo);
+        Assertions.assertThat(order.billing()).isEqualTo(billing);
     }
 
     @Test
@@ -152,7 +132,7 @@ class OrderTest {
                 .state("South Carolina")
                 .zipCode(new ZipCode("79911")).build();
 
-        BillingInfo billingInfo = BillingInfo.builder()
+        Billing billing = Billing.builder()
                 .address(address)
                 .document(new Document("225-09-1992"))
                 .phone(new Phone("123-111-9911"))
@@ -160,16 +140,16 @@ class OrderTest {
                 .build();
 
         Order order = Order.draft(new CustomerId());
-        order.changeBilling(billingInfo);
+        order.changeBilling(billing);
 
-        BillingInfo expectedBillingInfo = BillingInfo.builder()
+        Billing expectedBilling = Billing.builder()
                 .address(address)
                 .document(new Document("225-09-1992"))
                 .phone(new Phone("123-111-9911"))
                 .fullName(new FullName("John", "Doe"))
                 .build();
 
-        Assertions.assertThat(order.billing()).isEqualTo(expectedBillingInfo);
+        Assertions.assertThat(order.billing()).isEqualTo(expectedBilling);
 
     }
 
@@ -184,7 +164,7 @@ class OrderTest {
         Order order = Order.draft(new CustomerId());
 
         Assertions.assertThatExceptionOfType(OrderInvalidShippingDeliveryDateException.class)
-                .isThrownBy(()-> order.changeShipping(shipping));
+                .isThrownBy(() -> order.changeShipping(shipping));
     }
 
     @Test
