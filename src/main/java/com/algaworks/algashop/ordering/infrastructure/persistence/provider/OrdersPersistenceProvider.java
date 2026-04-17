6d00856bcc641aa37504bb08a41d0f67cf2b52c2
anailson <arsantos315@gmail.com>
2026-04-16 21:00:56 -0300

    Consultas para listagens

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.provider;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.repository.Orders;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
 import com.algaworks.algashop.ordering.infrastructure.persistence.assembler.OrderPersistenceEntityAssembler;
 import com.algaworks.algashop.ordering.infrastructure.persistence.disassembler.OrderPersistenceEntityDisassembler;
@@ -15,7 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
+import java.time.OffsetDateTime;
+import java.time.Year;
+import java.time.ZoneOffset;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Component
@@ -58,6 +64,20 @@ public class OrdersPersistenceProvider implements Orders {
                 );
     }
 
+    @Override
+    public List<Order> placeByCustomerInYEar(CustomerId customerId, Year year) {
+        OffsetDateTime start = year.atDay(1).atStartOfDay().atOffset(ZoneOffset.UTC);
+        OffsetDateTime end = start.plusYears(1).minusNanos(1);
+
+        List<OrderPersistenceEntity> entities = persistenceRepository.findByCustomer_IdAdndPlaceatBetween(
+                customerId.value(),
+                start,
+                end
+        );
+
+        return entities.stream().map(disassembler::toDomainEntity).collect(Collectors.toList());
+    }
+
 
 
     private void update(Order aggregateRoot, OrderPersistenceEntity persistenceEntity) {
@@ -82,6 +102,7 @@ public class OrdersPersistenceProvider implements Orders {
         ReflectionUtils.setField(version, aggregateRoot, persistenceEntity.getVersion());
         version.setAccessible(false);
     }
+
 
 
 }
