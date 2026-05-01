9b90209d3224cb5070047b0e8c704e0c31d5dd37
anailson <arsantos315@gmail.com>
2026-05-01 20:52:16 -0300

    Modularização na camada de Application

--- src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerOutput.java
+++ src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerOutput.java
@@ -1,24 +1,32 @@
-package com.algaworks.algashop.ordering.application.model;
+package com.algaworks.algashop.ordering.application.customer.management;
 
+import com.algaworks.algashop.ordering.application.commons.AddressData;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Data
 @NoRepositoryBean
 @AllArgsConstructor
 @Builder
-public class CustomerInput {
+public class CustomerOutput {
+
+    private UUID id;
     private String firstName;
     private String lastName;
     private String email;
-    private String phone;
     private String document;
+    private String phone;
     private LocalDate birthDate;
+    private Integer loyaltyPoints;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
     private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
     private AddressData address;
-
 }
